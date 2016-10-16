aeaa9a8a8c2b37d8ace9d1f49e0a22a47ff89991
HONG ZHOU <holly_zhou@yahoo.com>
2016-10-15 21:52:49 -0700

    make places api work

--- src/API.js
+++ src/API.js
@@ -6,9 +6,26 @@ const API = {
     axios.get(`/api/googleApi/directions?${query}`)
       .then(response => {
         let data = response.data
-        let latLon = data.routes[0].legs[0].steps
-        console.log('latLon:', latLon)
         ServerAction.receiveSearchResults(data)
+        let latLon = data.routes[0].legs[0].steps
+        return latLon
+      })
+      .then(latLon => {
+        latLon.map(pinPoint => {
+          if(pinPoint.distance.value > 6000) {
+            let { lat, lng } = pinPoint.end_location
+            let query = `location=${lat},${lng}&radius=500&type=gas_stations&keyword=gas`
+            API.getPlaces(query)
+          }
+        })
+      })
+      .catch(console.error)
+  },
+  getPlaces (query) {
+    
+    axios.get(`/api/googleApi/places?${query}`)
+      .then(res => {
+        console.log('res.data:', res.data)
       })
       .catch(console.error)
   }
